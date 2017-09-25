6d0e657c857d8fbf058886dc6fd306627877c597
wiatanos <wiatan.o.s@outlook.com>
2017-09-25 16:58:01 -0300

    add lat long

--- db/mail.php
+++ db/mail.php
@@ -1,9 +1,14 @@
 <?php 
 	include 'connect.php';
 
-	$stmt = $conn->prepare('INSERT INTO `contato`(nome, email, assunto, mensagem) VALUES(:nome, :email, :assunto, :mensagem)');
+	$stmt = $conn->prepare('INSERT INTO `contato`(nome, endereco, bairro, cep, lat, lng, email, assunto, mensagem) VALUES(:nome, :endereco, :bairro, :cep, :lat, :lng, :email, :assunto, :mensagem)');
 
 	 $stmt->bindParam(':nome', $_POST['nome']);
+	 $stmt->bindParam(':endereco', $_POST['endereco']);
+	 $stmt->bindParam(':bairro', $_POST['bairro']);
+	 $stmt->bindParam(':cep', $_POST['cep']);
+	 $stmt->bindParam(':lat', $_POST['lat']);
+	 $stmt->bindParam(':lng', $_POST['lng']);
 	 $stmt->bindParam(':email', $_POST['email']);
 	 $stmt->bindParam(':assunto', $_POST['assunto']);
 	 $stmt->bindParam(':mensagem', $_POST['mensagem']);
