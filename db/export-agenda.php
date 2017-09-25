0ac4d93a7054c7d8b6d907797c4cd20a90cb8d3d
wiatanos <wiatan.o.s@outlook.com>
2017-09-25 16:58:29 -0300

    definodo charset, add lat long

--- db/export-agenda.php
+++ db/export-agenda.php
@@ -7,19 +7,19 @@ $query->execute();
 $rows = ($query->fetchAll());
 
     echo "<table  border=\"1\" cellspacing=\"0\" bordercolor=\"#222\" id=\"list\">";
-    echo "<tr><th>Nome</th><th>email</th><th>telefone</th><th>endereço</th><th>bairro</th><th>cep</th><th>lat</th><th>long</th><th>peso</th>";
+    echo "<tr><th>Nome</th><th>email</th><th>telefone</th><th>".mb_convert_encoding('endereço', 'utf-16', 'utf-8')."</th><th>bairro</th><th>cep</th><th>lat</th><th>long</th><th>peso</th>";
 
 foreach ($rows as $key => $value) {
     echo "<tr>";
-    echo '<td>'.$value["nome_empresa"].'</td>';
-    echo '<td>'.$value["email"].'</td>';
-    echo '<td>'.$value["telefone"].'</td>';
-    echo '<td>'.$value["endereco"].'</td>';
-    echo '<td>'.$value["bairro"].'</td>';
-    echo '<td>'.$value["cep"].'</td>';
-    echo '<td>'.$value["lat"].'</td>';
-    echo '<td>'.$value["lng"].'</td>';
-    echo '<td>'.$value["peso"].'</td>';
+    echo '<td>'.mb_convert_encoding($value["nome_empresa"], 'utf-16', 'utf-8').'</td>';
+    echo '<td>'.mb_convert_encoding($value["email"],        'utf-16', 'utf-8').'</td>';
+    echo '<td>'.mb_convert_encoding($value["telefone"],     'utf-16', 'utf-8').'</td>';
+    echo '<td>'.mb_convert_encoding($value["endereco"],     'utf-16', 'utf-8').'</td>';
+    echo '<td>'.mb_convert_encoding($value["bairro"],       'utf-16', 'utf-8').'</td>';
+    echo '<td>'.mb_convert_encoding($value["cep"],          'utf-16', 'utf-8').'</td>';
+    echo '<td>'.mb_convert_encoding($value["lat"],          'utf-16', 'utf-8').'</td>';
+    echo '<td>'.mb_convert_encoding($value["lng"],          'utf-16', 'utf-8').'</td>';
+    echo '<td>'.mb_convert_encoding($value["peso"],         'utf-16', 'utf-8').'</td>';
     echo "</tr>";
 }
     echo "</table>";
@@ -30,6 +30,7 @@ header ("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
 header ("Cache-Control: no-cache, must-revalidate");
 header ("Pragma: no-cache");
 header ("Content-type: application/x-msexcel");
+header ("Charset: utf-8");
 header ("Content-Disposition: attachment; filename={$arquivo}" );
 header ("Content-Description: PHP Generated Data" );
 
--- db/export-contato.php
+++ db/export-contato.php
@@ -7,14 +7,17 @@ $query->execute();
 $rows = ($query->fetchAll());
 
 echo "<table  border=\"1\" cellspacing=\"0\" bordercolor=\"#222\" id=\"list\">";
-echo "<tr><th>Nome</th><th>email</th><th>Assunto</th><th>mensagem</th></tr>";
+echo "<tr><th>Nome</th><th>email</th><th>".mb_convert_encoding('Endereço','utf-16','utf-8')."</th><th>bairro</th><th>cep</th><th>Assunto</th><th>mensagem</th></tr>";
 
 foreach ($rows as $key => $value) {
 	echo "<tr>";
-	echo '<td>'.$value["nome"].'</td>';
-	echo '<td>'.$value["email"].'</td>';
-	echo '<td>'.$value["assunto"].'</td>';
-	echo '<td>'.$value["mensagem"].'</td>';
+	echo '<td>'.mb_convert_encoding($value["nome"],     'utf-16', 'utf-8').'</td>';
+	echo '<td>'.mb_convert_encoding($value["email"],    'utf-16', 'utf-8').'</td>';
+	echo '<td>'.mb_convert_encoding($value["endereco"], 'utf-16', 'utf-8').'</td>';
+	echo '<td>'.mb_convert_encoding($value["bairro"],   'utf-16', 'utf-8').'</td>';
+	echo '<td>'.mb_convert_encoding($value["cep"],      'utf-16', 'utf-8').'</td>';
+	echo '<td>'.mb_convert_encoding($value["assunto"], 	'utf-16', 'utf-8').'</td>';
+	echo '<td>'.mb_convert_encoding($value["mensagem"], 'utf-16', 'utf-8').'</td>';
 	echo "</tr>";
 }
 echo "</table>";
@@ -24,6 +27,7 @@ header ("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
 header ("Cache-Control: no-cache, must-revalidate");
 header ("Pragma: no-cache");
 header ("Content-type: application/x-msexcel");
+header ("Charset: utf-8");
 header ("Content-Disposition: attachment; filename={$arquivo}" );
 header ("Content-Description: PHP Generated Data" );
 ?>
