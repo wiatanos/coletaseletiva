6b39cffd4d769bbb3bf74df2c209c113eadc6985
wiatanos <wiatan.o.s@outlook.com>
2017-09-25 13:44:10 -0300

    melhorado o codigo

--- js/main.js
+++ js/main.js
@@ -22,74 +22,67 @@ function initialize() {
 		map: map
 	});
 
-		//Resize Function
-		// google.maps.event.addDomListener(window, "resize", function() {
-		// 	var center = map.getCenter();
-		// 	google.maps.event.trigger(map, "resize");
-		// 	map.setCenter(center);
-		// });
-
-		console.log(ctaLayer);
+	console.log(ctaLayer);
 
 
-		// Create the search box and link it to the UI element.
-		var input = document.getElementById('pac-input');
-		var searchBox = new google.maps.places.SearchBox(input);
-		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+	// Create the search box and link it to the UI element.
+	var input = document.getElementById('pac-input');
+	var searchBox = new google.maps.places.SearchBox(input);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-  	searchBox.setBounds(map.getBounds());
-  });
+	map.addListener('bounds_changed', function() {
+	 searchBox.setBounds(map.getBounds());
+	});
 
-  var markers = [];
+  	var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
-  	var places = searchBox.getPlaces();
+  	searchBox.addListener('places_changed', function() {
+  		var places = searchBox.getPlaces();
 
-  	if (places.length == 0) {
-  		return;
-  	}
+  		if (places.length === 0) {
+  			return;
+  		}
 
     // Clear out the old markers.
-    markers.forEach(function(marker) {
-    	marker.setMap(null);
-    });
-    markers = [];
+	    markers.forEach(function(marker) {
+	    	marker.setMap(null);
+	    });
+	    markers = [];
 
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-    	if (!place.geometry) {
-    		console.log("Returned place contains no geometry");
-    		return;
-    	}
-    	var icon = {
-    		url: place.icon,
-    		size: new google.maps.Size(71, 71),
-    		origin: new google.maps.Point(0, 0),
-    		anchor: new google.maps.Point(17, 34),
-    		scaledSize: new google.maps.Size(25, 25)
-    	};
+	    // For each place, get the icon, name and location.
+	    var bounds = new google.maps.LatLngBounds();
+	    places.forEach(function(place) {
+	    	if (!place.geometry) {
+	    		console.log("Returned place contains no geometry");
+	    		return;
+	    	}
+	    	var icon = {
+	    		url: place.icon,
+	    		size: new google.maps.Size(71, 71),
+	    		origin: new google.maps.Point(0, 0),
+	    		anchor: new google.maps.Point(17, 34),
+	    		scaledSize: new google.maps.Size(25, 25)
+	    	};
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-      	map: map,
-      	icon: icon,
-      	title: place.name,
-      	position: place.geometry.location
-      }));
+	      // Create a marker for each place.
+	      markers.push(new google.maps.Marker({
+	      	map: map,
+	      	icon: icon,
+	      	title: place.name,
+	      	position: place.geometry.location
+	      }));
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-    } else {
-    	bounds.extend(place.geometry.location);
-    }
-});
-    map.fitBounds(bounds);
-});
+	      if (place.geometry.viewport) {
+		       // Only geocodes have viewport.
+		       bounds.union(place.geometry.viewport);
+		   } else {
+		    bounds.extend(place.geometry.location);
+		   }
+		});
+    	map.fitBounds(bounds);
+	});
 }
 
 $( document ).ready( function() {
@@ -99,7 +92,7 @@ $( document ).ready( function() {
 	function buscaEndereco() {
 		var address = $('#bairro').val() +','+ $('#endereco').val() + ', Caraguatatuba - SP';
 		geocoder.geocode( { 'address': address}, function(results, status) {
-			if (status == 'OK') {
+			if (status === 'OK') {
 				$('#lat').val(results[0].geometry.location.lat());
 				$('#lng').val(results[0].geometry.location.lng());
 			}
@@ -112,24 +105,6 @@ $( document ).ready( function() {
 
 });
 
-//ancora
-
-// function scrollNav() {
-// 	$('a').click(function(){
-//     //Toggle Class
-//     $(".active").removeClass("active");
-//     $(this).closest('li').addClass("active");
-//     var theClass = $(this).attr("class");
-//     $('.'+theClass).parent('li').addClass('active');
-//     //Animate
-//     $('html, body').stop().animate({
-//     	scrollTop: $( $(this).attr('href') ).offset().top - 160
-//     }, 400);
-//     return false;
-// });
-// 	$('.scrollTop a').scrollTop();
-// }
-// scrollNav();
 
 //nav
 
@@ -297,10 +272,10 @@ $(document).ready(function () {
 //conato change
 
 $('#select_contato').on('change', function(){
-	if ($(this).val() == '2') {
+	if ($(this).val() === '2') {
 		$("#form_contato").removeAttr('hidden');
 		$("#form_agenda").attr('hidden', 'true');
-	}else if($(this).val() == '1'){
+	}else if($(this).val() === '1'){
 		$("#form_agenda").removeAttr('hidden');
 		$("#form_contato").attr('hidden', 'true');
 	}else{
